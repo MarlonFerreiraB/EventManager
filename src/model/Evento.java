e326ad91cce3073ebf4013b02d92981a5e9ac5da
Marlon Ferreira <mferreirab03@gmail.com>
2025-07-03 16:46:14 -0300

    feat: Implement Evento class with attributes and participant management

--- src/model/Evento.java
+++ src/model/Evento.java
@@ -1,4 +1,49 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class Evento {
+    private String name;
+    private String ID;
+    private LocalDate localDate;
+    private int capacidadeMax;
+    Set<Usuario> participantes;
+
+    public Evento(String name, LocalDate localDate, int capacidadeMax) {
+        this.localDate = localDate;
+        this.capacidadeMax = capacidadeMax;
+        this.participantes = new HashSet<>();
+        this.ID = UUID.randomUUID().toString();
+    }
+
+    public boolean addParticipante(Usuario user){
+        if(participantes == null){
+            return false;
+        }
+        participantes.add(user);
+        return true;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public int getCapacidadeMax() {
+        return capacidadeMax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public Set<Usuario> getParticipantes() {
+        return participantes;
+    }
 }
